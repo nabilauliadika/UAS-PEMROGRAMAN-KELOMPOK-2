86ccd60dbf9be8f6d854cb9daf2e2509d44f5e8a
raymondv01 <raymond25@mhs.usk.ac.id>
2025-11-20 21:17:05 +0700

    Refactor to use hash table for unique word detection
    Refactor word storage to use a hash table for unique word detection, improving efficiency. Added functions for hashing, checking existence, and saving words.

--- Bonus.c
+++ Bonus.c
@@ -4,95 +4,117 @@
 #include <stdlib.h>
 
 #define MAX_WORDS 5000
-#define MAX_LEN 100
+#define MAX_LEN   100
+#define HASH_SIZE 10007   // prime number for hashing
 
-int isValidChar(char c) {
-    return isalnum(c) || c == '\'';
+// Hash table untuk deteksi kata unik (lebih cepat dari linear search)
+char hashTable[HASH_SIZE][MAX_LEN] = {0};
+
+unsigned int hashFunc(const char *s) {
+    unsigned long h = 5381;
+    int c;
+    while ((c = *s++)) h = ((h << 5) + h) + c;
+    return h % HASH_SIZE;
 }
 
-void normalizeApostrophe(char *line) {
-    int i;
-    for (i = 0; line[i]; i++) {
-        // ganti typographic apostrophe ’ menjadi '
-        if (line[i] == '\xE2' && line[i+1] == '\x80' && line[i+2] == '\x99') {
-            line[i] = '\'';
-            memmove(&line[i+1], &line[i+3], strlen(&line[i+3]) + 1);
-        }
+int wordExists(const char *w) {
+    unsigned int idx = hashFunc(w);
+    return strcmp(hashTable[idx], w) == 0;
+}
+
+void saveWord(const char *w) {
+    unsigned int idx = hashFunc(w);
+    strcpy(hashTable[idx], w);
+}
+
+int isValidChar(char c) {
+    return isalnum((unsigned char)c) || c == '\'';
+}
+
+void normalizeApostrophe(char *s) {
+    char *p;
+    while ((p = strstr(s, "\xE2\x80\x99"))) {
+        *p = '\'';
+        memmove(p + 1, p + 3, strlen(p + 3) + 1);
     }
 }
 
-void toLowerStr(char *s) {
-    int i;
-    for (i = 0; s[i]; i++)
-        s[i] = tolower((unsigned char)s[i]);
-}
+void cleanApostrophe(char *s) {
+    int len = strlen(s);
 
-int exists(char arr[][MAX_LEN], int count, const char *w) {
-    int i;
-    for (i = 0; i < count; i++)
-        if (strcmp(arr[i], w) == 0)
-            return 1;
-    return 0;
+    // remove leading '
+    while (s[0] == '\'' && len > 1) {
+        memmove(s, s + 1, len);
+        len--;
+    }
+
+    // remove trailing '
+    while (len > 1 && s[len - 1] == '\'') {
+        s[--len] = '\0';
+    }
 }
 
 int main() {
     FILE *in = fopen("lirik.txt", "r");
     FILE *out = fopen("kosa-kata.word", "w");
 
-    char line[1000];
-    char words[MAX_WORDS][MAX_LEN];
-    int wc = 0;
-    int firstLine = 1;
-
     if (!in || !out) {
         printf("Gagal membuka file.\n");
         return 1;
     }
 
+    char line[1000];
+    int firstLine = 1;
+
     while (fgets(line, sizeof(line), in)) {
 
-        normalizeApostrophe(line); // FIX penting!
+        normalizeApostrophe(line);
 
+        // baris pertama langsung dicetak
         if (firstLine) {
             fprintf(out, "%s", line);
             firstLine = 0;
             continue;
         }
 
-        int len = strlen(line), i, idx = 0;
+        int i = 0, idx = 0;
         char temp[MAX_LEN];
 
-        for (i = 0; i <= len; i++) {
+        while (1) {
+            char c = line[i];
 
-            if (isValidChar(line[i])) {
-                temp[idx++] = line[i];
+            if (isValidChar(c)) {
+                if (idx < MAX_LEN - 1)
+                    temp[idx++] = c;
             } else {
                 if (idx > 0) {
                     temp[idx] = '\0';
                     idx = 0;
 
-                    // hapus apostrof di awal/akhir kata jika ada
-                    while (temp[0] == '\'' && strlen(temp) > 1)
-                        memmove(temp, temp + 1, strlen(temp));
+                    cleanApostrophe(temp);
 
-                    while (temp[strlen(temp) - 1] == '\'' && strlen(temp) > 1)
-                        temp[strlen(temp) - 1] = '\0';
+                    for (int k = 0; temp[k]; k++)
+                        temp[k] = tolower((unsigned char)temp[k]);
 
-                    toLowerStr(temp);
-
-                    if (!exists(words, wc, temp))
-                        strcpy(words[wc++], temp);
+                    if (!wordExists(temp))
+                        saveWord(temp);
                 }
             }
+
+            if (c == '\0')
+                break;
+            i++;
         }
     }
 
-    int i;
-    for (i = 0; i < wc; i++)
-        fprintf(out, "%s=\n", words[i]);
+    // Output semua kata unik
+    for (int i = 0; i < HASH_SIZE; i++) {
+        if (hashTable[i][0]) {
+            fprintf(out, "%s=\n", hashTable[i]);
+        }
+    }
 
     fclose(in);
     fclose(out);
-
     return 0;
 }
