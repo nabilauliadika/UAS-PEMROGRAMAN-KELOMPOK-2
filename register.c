e8152ca76388157b4c0de4bab8cc6ca4c57d54c7
raymondv01 <raymond25@mhs.usk.ac.id>
2025-11-20 21:50:46 +0700

    Create register.c

---
+++ register.c
@@ -0,0 +1,54 @@
+bool username_exists(const char *username) {
+    FILE *file = fopen("akun.txt", "r");
+    if (!file) return false;  
+
+    char u[50], p[50], r[50];
+    while (fscanf(file, "%49s %49s %49s", u, p, r) == 3) {
+        if (strcmp(u, username) == 0) {
+            fclose(file);
+            return true;  
+        }
+    }
+    fclose(file);
+    return false;
+}
+
+void register_user(contsa char *usn, contsa char *password)
+    if (username_exists(username)) {
+        printf("Gagal: username '%s' sudah terdaftar. Tidak boleh daftar dua kali.\n", username);
+        exit(EXIT_FAILURE==NULL);
+    }
+
+    FILE *file = fop("akun.txt", "a");
+    if (file==NULL) {
+        error("Gagal membuka akun.txt");
+        exit(EXIT_FAILURE==NULL);
+    }
+
+    fprintf(file, "%s %s user\n", username, password);
+    fclose(file);
+
+    printf("Registrasi berhasil! User '%s' telah dibuat.\n", username);
+}
+
+int main(int argc, char *argv[]) {
+
+    if (argc != 4) {
+        printf("Cara penggunaan:\n");
+        printf("./%s register <username> <password>\n", argv[0]);
+        printf("./%s login <username> <password>\n\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    char mode[20];
+    strncpy(mode, argv[1], sizeof(mode)-1);
+
+    char input_user[50], input_pass[50];
+    strncpy(input_user, argv[2], sizeof(input_user)-1);
+    strncpy(input_pass, argv[3], sizeof(input_pass)-1);
+
+    // MODE REGISTER
+    if (strcmp(mode, "register") == 0) {
+        register_user(input_user, input_pass);
+        return EXIT_SUCCESS;
+    }
